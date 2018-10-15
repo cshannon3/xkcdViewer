0be91459f34e6b25408124f85c891e17912b3f79
cshannon3 <33521708+cshannon3@users.noreply.github.com>
2018-10-15 01:28:27 -0400

    ComicPage setup for multipe comics

--- lib/pages/comic_page.dart
+++ lib/pages/comic_page.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:xkcd/data/comic.dart';
+
 import 'package:xkcd/widgets/comic_view.dart';
 
 class ComicPage extends StatelessWidget {
-  final Comic comic;
+  final List<Comic> comics;
 
-  ComicPage(this.comic);
+  ComicPage({this.comics});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: ComicView(comic),
+        child: ComicsView(comics: comics),
       ),
     );
   }
 }
+
